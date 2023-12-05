6791328782a5df39c55e9ed955cd21591e2efa94
bverreman <benverreman@gmail.com>
2023-12-05 12:48:56 -0500

    tuto4: change bashrc config script

---
+++ Tutorial_4_Lab_computers/scripts/Neuro_iX_bashrc.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#########
+## FSL Setup
+#########
+FSLDIR=/project/Neuro-IX/software/fsl6
+. $FSLDIR/etc/fslconf/fsl.sh
+source /etc/environment #reset PATH
+PATH=$FSLDIR/share/fsl/bin:$PATH
+export FSLDIR PATH
+
+#########
+## Freesurfer Setup
+#########
+export FREESURFER_HOME=/project/Neuro-IX/software/freesurfer
+source $FREESURFER_HOME/SetUpFreeSurfer.sh
+
+#########
+## Slicer Setup
+#########
+export SITK_SHOW_COMMAND=/project/Neuro-IX/software/Slicer-5.6.0-linux-amd64
+alias slicer='/project/Neuro-IX/software/Slicer-5.6.0-linux-amd64/Slicer'
+
+#########
+## Miniconda3 Setup
+#########
+export PATH="/project/Neuro-IX/software/miniconda3/bin:/home/user/miniconda3/condabin:$PATH"
+source "/project/Neuro-IX/software/miniconda3/bin/activate"
+
--- Tutorial_4_Lab_computers/scripts/setup_bashrc.sh
+++ Tutorial_4_Lab_computers/scripts/setup_bashrc.sh
@@ -24,38 +24,11 @@ Echo()
 }
 
 #########
-## Add setups in $HOME/.bashrc script
+## Add setups by sourcing a script in $HOME/.bashrc
 #########
 Echo "
 #########
-## FSL Setup
+## Neuro-iX bashrc
 #########
-FSLDIR=/project/Neuro-IX/software/fsl6
-. \$FSLDIR/etc/fslconf/fsl.sh
-source /etc/environment #reset PATH
-PATH=\$FSLDIR/share/fsl/bin:\$PATH
-export FSLDIR PATH
-
-#########
-## Freesurfer Setup
-#########
-export FREESURFER_HOME=/project/Neuro-IX/software/freesurfer
-source \$FREESURFER_HOME/SetUpFreeSurfer.sh
-
-#########
-## Slicer Setup
-#########
-export SITK_SHOW_COMMAND=/project/Neuro-IX/software/Slicer-5.6.0-linux-amd64
-alias slicer='/project/Neuro-IX/software/Slicer-5.6.0-linux-amd64/Slicer'
-
-#########
-## Miniconda3 Setup
-#########
-export PATH="/project/Neuro-IX/software/miniconda3/bin:/home/user/miniconda3/condabin:\$PATH"
-source "/project/Neuro-IX/software/miniconda3/bin/activate"
+source /project/Neuro-IX/software/env/Neuro_iX_bashrc.sh
 "
-
-#########
-## Source $HOME/.bashrc script
-#########
-source $HOME/.bashrc
