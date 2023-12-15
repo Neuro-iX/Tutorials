bac2205b1e98a6c926b6c0d6494da608cb7f286d
Benoît Verreman <131895621+bverreman@users.noreply.github.com>
2023-12-15 17:42:45 -0500

    Add files via upload
    command lines for installing OR executing FastSurfer

--- Tutorial_4_Lab_computers/scripts/fastsurfer_on_milner_script.sh
+++ Tutorial_4_Lab_computers/scripts/fastsurfer_on_milner_script.sh
@@ -1,9 +1,23 @@
+#######
+#Installation of FastSurfer
+#######
+#git clone --branch stable https://github.com/Deep-MI/FastSurfer.git
+cd /project/Neuro-IX/software/FastSurfer
+git fetch origin
+git merge origin/main
+
+conda remove -n fastsurfer_gpu --all
+conda env create -f /project/Neuro-IX/software/FastSurfer/fastsurfer_env_gpu.yml
+
+#sudo apt install tcsh
+#conda install DONT WORK
+
+#######
+#Execution of FastSurfer
+#######
 conda activate fastsurfer_gpu
 
 export FREESURFER_HOME=/project/Neuro-IX/software/freesurfer732
 source $FREESURFER_HOME/SetUpFreeSurfer.sh
 
-#sudo apt install tcsh
-#conda install DONT WORK
-
-fastsurfer --fs_license /project/Neuro-IX/software/freesurfer732/license.txt --sid HCP_1200_100307_v5 --sd ~/Documents/fastsurferTest --t1 /project/Neuro-IX/data/HCP_1200_structural_extended_preprocessed/100307/T1w/100307/mri/orig/001.mgz 
+fastsurfer --fs_license /project/Neuro-IX/software/freesurfer732/license.txt --sid HCP_1200_100307_fs_test --sd ~/Documents/fastsurferTest --t1 /project/Neuro-IX/data/HCP_1200_structural_extended_preprocessed/100307/T1w/100307/mri/orig/001.mgz 
