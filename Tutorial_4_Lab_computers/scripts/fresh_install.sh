35fd40c4ba086109b3edeef934aa0aeeb1c20c91
Benoît Verreman <131895621+bverreman@users.noreply.github.com>
2024-01-09 14:16:29 -0500

    tuto4: Update fresh_install.sh: little corrections

--- Tutorial_4_Lab_computers/scripts/fresh_install.sh
+++ Tutorial_4_Lab_computers/scripts/fresh_install.sh
@@ -2,7 +2,7 @@
 # SSHFS
 ######
 
-apt install sshfsapt install sshfs
+apt install sshfs
 
 ######
 # VScode 
@@ -115,33 +115,33 @@ mv $HOME/Zotero_linux-x86_64 $HOME/Zotero
 ######
 
 # Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
+apt-get update
+apt-get install ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
 
 # Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin #dependencies
-sudo docker run hello-world #testing installation
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin #dependencies
+docker run hello-world #testing installation
 #'docker' in terminal
 
 ######
 # Apptainer
 ######
 
-sudo apt update
-sudo apt install -y software-properties-common
+apt update
+apt install -y software-properties-common
 
-sudo add-apt-repository -y ppa:apptainer/ppa
-sudo apt update
-sudo apt install -y apptainer
+add-apt-repository -y ppa:apptainer/ppa
+apt update
+apt install -y apptainer
 
 ############################
 #EXTRA
