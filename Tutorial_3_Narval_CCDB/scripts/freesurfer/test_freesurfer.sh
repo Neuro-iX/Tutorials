636d2d432ae214f83e7440992d01ade7e8287200
Benoît Verreman <131895621+bverreman@users.noreply.github.com>
2024-01-31 17:14:13 -0500

    tuto3: Update test_freesurfer.sh

--- Tutorial_3_Narval_CCDB/scripts/freesurfer/test_freesurfer.sh
+++ Tutorial_3_Narval_CCDB/scripts/freesurfer/test_freesurfer.sh
@@ -1,10 +1,11 @@
 #!/bin/bash 
 
+### SUBJECTS_DIR is the variable used by freesurfer to set the output directory path
 export SUBJECTS_DIR=/mnt/software/Freesurfer7.4.1_container
 
-if [ -d "$SUBJECTS_DIR/bert" ] #bert folder does exist already
+if [ -d "$SUBJECTS_DIR/bert" ] ### Check if subject ("-s") 'bert' (=output folder) does exist already, and if not, give the raw_image path using "-i"
 then
-	  recon-all -autorecon1 -s bert
+	  recon-all -autorecon1 -s bert ### Execute freesurfer pipeline step 1 (autorecon1 = skull-strip)
 else
     recon-all -autorecon1 -s bert -i /usr/local/freesurfer/subjects/bert/mri/orig/001.mgz
 fi
