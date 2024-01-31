7f66c2077e782b618e775804d5bbee2b05d230c3
Benoît Verreman <131895621+bverreman@users.noreply.github.com>
2024-01-31 18:22:15 -0500

    tuto3: Update fastsurfer_job.sh: all in one script

--- Tutorial_3_Narval_CCDB/scripts/fastsurfer/fastsurfer_job.sh
+++ Tutorial_3_Narval_CCDB/scripts/fastsurfer/fastsurfer_job.sh
@@ -4,13 +4,13 @@
 #SBATCH --job-name fastsurfer_job
 #SBATCH --output=output_fastsurfer_%j.txt
 #SBATCH --open-mode=append
+#SBATCH --mem-per-cpu=4000 
 
 ### '#SBATCH ...' are directives (or options) in the job script (must precede all executable commands)
 ### Other examples:
 # #SBATCH --nodes=1
 # #SBATCH --ntasks-per-node=1
 # #SBATCH --ntasks=1
-# #SBATCH --mem-per-cpu=4000 
 # #SBATCH --mail-user=...@...
 # #SBATCH --mail-type=ALL
 
@@ -26,10 +26,10 @@ LAST UPDATE: 2024-01-15
 
 DESCRIPTION: 
 On Narval computing server, use apptainer and the image fastsurfer-gpu.sif to create a container, and execute a second script inside.
-You have to add the path to the second script to be executed inside the generated container.
 
 EXAMPLE OF USAGE:
-sbatch ~/projects/def-sbouix/software/fastsurfer/fastsurfer_job.sh ~/projects/def-sbouix/software/fastsurfer/test_fastsurfer.sh
+cd ~/projects/def-sbouix/software/fastsurfer
+sbatch fastsurfer_job.sh
 "
 }
 
@@ -40,28 +40,29 @@ sbatch ~/projects/def-sbouix/software/fastsurfer/fastsurfer_job.sh ~/projects/de
 ### Load the software apptainer version 1.1.8 onto your Narval login node session
 module load apptainer/1.1.8
 
-if [[ -z "$1"  ||  "$1" == "-h" ||  "$1" == "--help"]] ### First variable is empty or need help
-then
+if [[ -z "$1"  ||  "$1" == "-h" ||  "$1" == "--help"]]; then ### First variable is empty or need help
 	Help ### Activate Help function
 else ### You did add the second script to be executed inside the container
 	echo "---Start container from fastsurfer_gpu.sif" 
-	var=`echo $1 | awk -F 'def-sbouix/' '{print $2}'` ### Get the name of the second script to be executed inside the container
  
 	### launch a job of creating a container using apptainer and the image file "fastsurfer-gpu.sif"
  	### --nv flag means enabling the use of the gpu
   	### --no-hone flag means not mounting the $HOME directory (add a pointer to $HOME inside the container)
    	### --bind flag means mounting a certain folder/file inside the container
     	### You need to mount freesurfer license.txt, the output directory, the path to your raw_data
-     	### bash is used to launch the second script inside the container
+     	### bash is used to launch Fastsurfer script
  	srun apptainer exec --nv \
 			--no-home \
 			--bind /lustre06/project/6074560/data/AMPSCZ/rawdata/sub-BI02450/ses-202304111/anat/sub-BI02450_ses-202304111_rec-nonnorm_run-1_T1w_auxiliary.nii.gz:/data \
 			--bind /scratch/bverre:/output \
 			--bind /lustre06/project/6074560/software/fastsurfer/license.txt:/fs_license
 			/project/def-sbouix/software/fastsurfer/fastsurfer-gpu.sif \
-			bash /mnt/$var
+			bash /fastsurfer/run_fastsurfer.sh --sid FastSurferTest \
+			--sd /output \
+			--t1 /data \
+			--fs_license /fs_license \
+			--parallel --3T
 	wait ### wait for the container to do its job before being killed
 	echo "---End container from fastsurfer_gpu.sif"
-fi
 
 exit 0
