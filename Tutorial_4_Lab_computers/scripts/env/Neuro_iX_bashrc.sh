53e5ec2c4a67b0fc3233661483038a28cf51659f
Benoît Verreman <131895621+bverreman@users.noreply.github.com>
2024-10-07 15:35:18 -0400

    Update Neuro_iX_bashrc.sh

--- Tutorial_4_Lab_computers/scripts/env/Neuro_iX_bashrc.sh
+++ Tutorial_4_Lab_computers/scripts/env/Neuro_iX_bashrc.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+#/project/Neuro-IX/software... with previous server (Miller)
+# /mnt/shared_data/softwares for hippocampus
+
 #########
 ## FSL Setup
 #########
-FSLDIR=/project/Neuro-IX/software/fsl6
+FSLDIR=/mnt/shared_data/softwares/fsl
 . $FSLDIR/etc/fslconf/fsl.sh
 source /etc/environment #reset PATH
 PATH=$FSLDIR/share/fsl/bin:/usr/share:$PATH
@@ -12,35 +15,11 @@ export FSLDIR PATH
 #########
 ## Freesurfer Setup
 #########
-export FREESURFER_HOME=/project/Neuro-IX/software/freesurfer
+export FREESURFER_HOME=/mnt/shared_data/softwares/freesurfer
 source $FREESURFER_HOME/SetUpFreeSurfer.sh
 
 #########
 ## Slicer Setup
 #########
-export SITK_SHOW_COMMAND=/project/Neuro-IX/software/Slicer-5.6.1-linux-amd64
+export SITK_SHOW_COMMAND=/mnt/shared_data/softwares/slicer
 alias slicer='$SITK_SHOW_COMMAND/Slicer'
-
-#########
-## Miniconda3 Setup
-#########
-export PATH="/project/Neuro-IX/software/miniconda3/bin:/home/user/miniconda3/condabin:$PATH"
-source "/project/Neuro-IX/software/miniconda3/bin/activate"
-
-#########
-## FastSurfer
-#########
-export FASTSURFER_HOME=/project/Neuro-IX/software/FastSurfer
-alias fastsurfer='$FASTSURFER_HOME/run_fastsurfer.sh'
-export PATH="/project/Neuro-IX/software/miniconda3/bin/python:/project/Neuro-IX/software/FastSurfer:$PATH"
-
-#########
-## Anydesk
-#########
-alias anydesk='/project/Neuro-IX/software/anydesk-6.3.0/anydesk'
-
-#########
-## Globus
-#########
-alias globus='bash /project/Neuro-IX/software/globusconnectpersonal-3.2.3/globusconnectpersonal'
-
